0a90bfcbe5993638fa4a20694d3153bf22b0dfbe
shixiangzhao <zhaoshixiang@bingofresh.com>
2018-05-03 20:36:48 +0800

    multiple modules for java project

---
+++ bcms-po/src/main/java/org/bcms/po/App.java
@@ -0,0 +1,13 @@
+package org.bcms.po;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
